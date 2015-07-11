d06203624113f788399dcad55e30bea978e41fab
Sergio Bellido <sbellidov@gmail.com>
2015-07-11 18:03:05 +0200

    1.0 (Ejercicio obligatorio Tema 6)
    -Añadidos estilos css para smartphones y pantalla ancha. Responsive
    design.
    -Añadida la página de ‘Autor’ con foto.
    -Formato rediseñado para los menús y elementos del formulario de
    respuesta.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,21 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer  );
+
+
+/* GET author page. */
+router.get('/author', function(req, res) {
+  res.render('author', {});
+});
 
 
 module.exports = router;
