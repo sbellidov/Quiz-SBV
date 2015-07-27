3e237a65f43a5fd53500ab4556cf1038b0c42335
Sergio Bellido <sbellidov@gmail.com>
2015-07-27 23:34:00 +0200

    1.1e

--- models/models.js
+++ models/models.js
@@ -28,8 +28,8 @@ var sequelize = new Sequelize (DB_name, user, pwd, {
 
 // Usar BBDD SQLite
 // var sequelize = new Sequelize(null,null,null,{
-   dialect: "sqlite", storage: "quiz.sqlite"
-});
+//   dialect: "sqlite", storage: "quiz.sqlite"
+//});
 
 // Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
