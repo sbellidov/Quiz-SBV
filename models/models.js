803420e1c1eff8f61911a7b674e52ceb13802260
Sergio Bellido <sbellidov@gmail.com>
2015-07-28 22:07:57 +0200

    1.1j
    corrección del filtro

--- models/models.js
+++ models/models.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 // Postgres DATABASE_URL = postgres:((user:passwd@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
-var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)@(.*)\:(.*)\/(.*)/);
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var DB_name = (url[6]||null);
 var user    = (url[2]||null);
 var pwd     = (url[3]||null);
