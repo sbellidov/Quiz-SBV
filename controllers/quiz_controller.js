095c91615136999a5de02937ce722f9ecb191bac
Sergio Bellido <sbellidov@gmail.com>
2015-08-01 13:30:37 +0200

    1.3
    -Añadido la función Autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,22 @@
 // quiz_controller.js
 var models = require('../models/models.js');
 
+// Autoload - factoriza el código si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+   models.Quiz.findById(quizId).then(
+      function(quiz){
+         if (quiz) {
+            req.quiz = quiz;
+            next();
+         } else {
+            next(new Error('No existe quizId=' + quizId));
+         }
+      }
+   ).catch(function(error) { next(error);});
+};
 
-//// GET /quizes/question
-//exports.question = function(req, res) {
-//    //res.render('quizes/question', {pregunta: 'Capital de Italia'});
-//   models.Quiz.findAll().then(function(quiz){
-//      res.render('quizes/question',{pregunta: quiz[0].pregunta})
-//   })
-//};
 
-// GET /quizes
+// GET /quizes (lista de quizes)
 exports.index = function(req, res) {
    models.Quiz.findAll().then(function(quizes){
       res.render('quizes/index.ejs',{ quizes: quizes});
@@ -20,20 +26,16 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-   models.Quiz.findById(req.params.quizId).then(function(quiz) {
-      res.render('quizes/show',{quiz: quiz });
-   })
+   res.render('quizes/show', {quiz: req.quiz});
 };
-
 
 
 // GET /quizes/:id/answer
 exports.answer = function(req, res) {
-   models.Quiz.findById(req.params.quizId).then(function(quiz){
-      if (req.query.respuesta === quiz.respuesta){
-         res.render('quizes/answer', {quiz: quiz , respuesta: 'Correcto'});
-      } else {
-         res.render('quizes/answer', {quiz: quiz , respuesta: 'Incorrecto'});
-      }
-   })
+   var resultado = 'Incorrecto';
+   if (req.query.respuesta === req.quiz.respuesta) {
+      resultado = 'Correcto';
+   }
+   res.render('quizes/answer', {quiz: req.quiz , respuesta: resultado})
+  
 };
