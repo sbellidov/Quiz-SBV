0f66624f9c7d5dcf46973e214cf1ce849d40c0d1
Sergio Bellido <sbellidov@gmail.com>
2015-08-03 00:15:19 +0200

    1.6
    -Añadida funcionalidad de editar pregunta
    -Añadida funcionalidad de borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -67,3 +67,38 @@ exports.create = function(req, res) {
       }
    );
 };
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+   var quiz = req.quiz;    //Toma el dato del Autoload
+   res.render('quizes/edit',{quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+   
+   req.quiz.pregunta    =  req.body.quiz.pregunta;
+   req.quiz.respuesta   =  req.body.quiz.respuesta;
+   
+   req.quiz
+   .validate()
+   .then(
+      function(err) {
+         if (err) {
+            res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+         } else {
+            req.quiz
+               // Guarda en la Base de Datos los campos pregunta y respuesta
+               .save({fields: ["pregunta", "respuesta"]})
+               .then(function(){res.redirect('/quizes')});
+         }
+      }
+   );
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+   req.quiz.destroy().then ( function() {
+      res.redirect('/quizes');
+   }).catch(function(error){next(error)});
+};
--- routes/index.js
+++ routes/index.js
@@ -17,15 +17,13 @@ router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
-
-//router.get('/quizes/question', quizController.question);
-//router.get('/quizes/answer',   quizController.answer  );
-
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
+router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
 /* GET author page. */
 router.get('/author', function(req, res) {
   res.render('author', {errors: []});
 });
-
 
 module.exports = router;
